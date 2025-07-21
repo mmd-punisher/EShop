1f9c6f10eefa1ce9ecbdd0a36701419d2f54e2b9
mmd-punisher <mohammadmoosapoor4@gmail.com>
2025-07-22 02:41:23 +0330

    update: Nested Serializers,  Serializer Relations, Aggregated API data

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -7,7 +7,7 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = (
             "name",
-            "description",
+            # "description",
             "price",
             "stock",
         )
@@ -16,3 +16,33 @@ class ProductSerializer(serializers.ModelSerializer):
         if value <= 0:
             raise serializers.ValidationError("Price must be grater than 0.")
         return value
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product_name = serializers.CharField(source="product.name")
+    product_price = serializers.DecimalField(
+        max_digits=10, decimal_places=2, source="product.price"
+    )
+
+    class Meta:
+        model = OrderItem
+        fields = ("product_name", "product_price", "quantity", "item_subtotal")
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField(method_name="total")
+
+    def total(elf, obj):
+        order_items = obj.items.all()
+        return sum(order_item.item_subtotal for order_item in order_items)
+
+    class Meta:
+        model = Order
+        fields = ("order_id", "user", "created_at", "status", "items", "total_price")
+
+
+class ProductInfoSerializer(serializers.Serializer):
+    products = ProductSerializer(many=True)
+    count = serializers.IntegerField()
+    max_price = serializers.FloatField()
--- core/api/views.py
+++ core/api/views.py
@@ -1,7 +1,8 @@
+from django.db.models import Max
 from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
-from api.serializers import ProductSerializer
-from api.models import Product
+from api.serializers import ProductSerializer, OrderSerializer, ProductInfoSerializer
+from api.models import Product, Order
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
@@ -13,8 +14,27 @@ def product_list(request):
     return Response(serializer.data)
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     serializer = ProductSerializer(product)
     return Response(serializer.data)
+
+
+@api_view(["GET"])
+def order_list(request):
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def product_info(request):
+    products = Product.objects.all()
+    serializer = ProductInfoSerializer({
+        'products': products,
+        'count': len(products),
+        'max_price': products.aggregate(max_price=Max('price'))['max_price']
+    })
+    return Response(serializer.data)
+
+
