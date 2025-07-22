e5090bfa49f108a7e56e3c07b71e59d842a8b4dc
mmd-punisher <mohammadmoosapoor4@gmail.com>
2025-07-23 02:53:13 +0330

    update: ListAPIView, RetrieveAPIView

--- core/api/views.py
+++ core/api/views.py
@@ -5,39 +5,32 @@ from api.serializers import ProductSerializer, OrderSerializer, ProductInfoSeria
 from api.models import Product, Order
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import generics
+
+
+class ProductListAPIView(generics.ListAPIView):
+    queryset = Product.objects.filter(stock__gt=0)
+    serializer_class = ProductSerializer
+
+
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class OrderListAPIView(generics.ListAPIView):
+    queryset = Order.objects.prefetch_related("items__product")
+    serializer_class = OrderSerializer
 
 
 @api_view(["GET"])
-def product_list(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
-
-
-@api_view(["GET"])
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
-
-
-@api_view(["GET"])
-def order_list(request):
-    # orders = Order.objects.prefetch_related(
-        # "items", "items__product"
-        # ).all()
-    orders = Order.objects.prefetch_related("items__product")
-    serializer = OrderSerializer(orders, many=True)
-    return Response(serializer.data)
-
-@api_view(['GET'])
 def product_info(request):
     products = Product.objects.all()
-    serializer = ProductInfoSerializer({
-        'products': products,
-        'count': len(products),
-        'max_price': products.aggregate(max_price=Max('price'))['max_price']
-    })
+    serializer = ProductInfoSerializer(
+        {
+            "products": products,
+            "count": len(products),
+            "max_price": products.aggregate(max_price=Max("price"))["max_price"],
+        }
+    )
     return Response(serializer.data)
-
-
