5c710d7baa07d9c7f7dd223f0925ea967304a30b
mmd-punisher <mohammadmoosapoor4@gmail.com>
2025-07-29 02:51:32 +0330

    update: views.py

--- core/api/urls.py
+++ core/api/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products/', views.ProductListAPIView.as_view()),
-    path('products/info/', views.product_info),
-    path('products/<int:pk>/', views.ProductDetailAPIView.as_view()),
-    path('orders/', views.OrderListAPIView.as_view()),
-    path('user-orders/', views.UserOrderListAPIView.as_view()),
+    path("products/", views.ProductListCreateAPIView.as_view()),
+    path("products/info/", views.ProductInfoAPIView.as_view()),
+    path("products/<int:pk>/", views.ProductDetailAPIView.as_view()),
+    path("orders/", views.OrderListAPIView.as_view()),
+    path("user-orders/", views.UserOrderListAPIView.as_view()),
 ]
